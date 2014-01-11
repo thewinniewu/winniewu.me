f5d19ddff8bc20320d5c98c8f3a106faa414da52
Winnie Wu <weiwu@college.harvard.edu>
2014-01-12 00:49:18 +1300

    animations

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,19 @@ var tmovx = 85; //need to change tsize in stylesheet.scss when changed
 var tmovy = tmovx * 2 * 0.869 - 1;
 var tmovx = tmovx + 2;
 
-var dOffsetsY = [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, 0, -1];
-var dOffsetsX = [0, 1, 3, 4, 7, 8, 1, 3, 5, 7, 2, 6];
+var dOffsetsY = [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, 0, -1];
+var dOffsetsX = [0, 1, 3, 4, 7, 8, 1, 2, 3, 5, 6, 7, 2, 6];
 
 $(document).ready( function() {
 
 var wrapper = document.getElementById("w-wrapper");
 
-for (var i = 0; i < 12; i++) {
+for (var i = 0; i < 14; i++) {
     var triangle = document.createElement("div");
-    if (i === 1 || i === 2 || i === 4) {
+    if (i === 1 || i === 2 || i === 4 || i === 7 || i === 10) {
         triangle.id = "triangle-up";
+        if (i === 7 || i === 10)
+            triangle.style.borderBottomColor = "rgba(255,255,255,0.5)";
     } 
     else {
         triangle.id = "triangle-down";
@@ -20,16 +22,20 @@ for (var i = 0; i < 12; i++) {
     
     triangle.style.marginTop = (tmovy * dOffsetsY[i]);
     triangle.style.marginLeft = (tmovx * dOffsetsX[i] - tmovx/2); 
+    
+   
     wrapper.appendChild(triangle);
 }
     var artlink = document.createElement("div");
     artlink.id = "artlink";
-    artlink.innerHTML = '<a href="#"> ART </a>'; 
+    artlink.innerHTML = '<a href="http://people.fas.harvard.edu/~weiwu/home.html" target=blank> ART </a>'; 
     wrapper.appendChild(artlink);
     
     var codelink = document.createElement("div");
     codelink.id = "codelink";
-    codelink.innerHTML ='<a href="#"> CODE </a>';
+    codelink.innerHTML ='<a href="http://www.github.com/thewinniewu" target=blank> CODE </a>';
     wrapper.appendChild(codelink);
+
+
 
 });
