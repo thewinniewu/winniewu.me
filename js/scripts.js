946b455d1f4e5db991c0911ac47c4eb731d88efb
Winnie Wu <weiwu@college.harvard.edu>
2014-01-11 23:55:14 +1300

    links positioned

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
-var tmovx = 80; //need to change tsize in stylesheet.scss when changed
-var tmovy = tmovx * 2 * 0.866 - 1;
+var tmovx = 85; //need to change tsize in stylesheet.scss when changed
+var tmovy = tmovx * 2 * 0.869 - 1;
 var tmovx = tmovx + 2;
 
-var dOffsetsY = [0, -1, -1, -1, -1, 0, -1, -1, -1, 0, -1];
-var dOffsetsX = [0, 1, 4, 3,  8, 1, 3, 5, 7, 2, 6];
+var dOffsetsY = [0, -1, -1, -1, -1, -1, 0, -1, -1, -1, 0, -1];
+var dOffsetsX = [0, 1, 3, 4, 7, 8, 1, 3, 5, 7, 2, 6];
 
 $(document).ready( function() {
 
 var wrapper = document.getElementById("w-wrapper");
 
-for (var i = 0; i < 11; i++) {
+for (var i = 0; i < 12; i++) {
     var triangle = document.createElement("div");
-    if (i === 1 || i === 3) {
+    if (i === 1 || i === 2 || i === 4) {
         triangle.id = "triangle-up";
     } 
     else {
         triangle.id = "triangle-down";
-    }
+    } 
+    
     triangle.style.marginTop = (tmovy * dOffsetsY[i]);
-    triangle.style.marginLeft = (tmovx * dOffsetsX[i]); 
+    triangle.style.marginLeft = (tmovx * dOffsetsX[i] - tmovx/2); 
     wrapper.appendChild(triangle);
 }
+    var artlink = document.createElement("div");
+    artlink.id = "artlink";
+    artlink.innerHTML = '<a href="#"> ART </a>'; 
+    wrapper.appendChild(artlink);
+    
+    var codelink = document.createElement("div");
+    codelink.id = "codelink";
+    codelink.innerHTML ='<a href="#"> CODE </a>';
+    wrapper.appendChild(codelink);
 
 });
